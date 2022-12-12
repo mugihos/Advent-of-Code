9b7561f631642a0fd6421e0fea30840cb138ac99
Mugiho <65477315+mugihos@users.noreply.github.com>
2022-12-12 14:20:09 +1300

    add working solution for part2

--- 2022/day1/day1.js
+++ 2022/day1/day1.js
@@ -1,9 +1,10 @@
+//setting up data for use
 const fs = require('fs')
-
 const data = fs.readFileSync('input.txt', 'utf8')
-
+const testData = fs.readFileSync('test.txt', 'utf8')
 const splitData = data.split('\n')
 
+//block total array
 const newArr = []
 let total = 0
 for (let i = 0; i < splitData.length; i++) {
@@ -14,5 +15,20 @@ for (let i = 0; i < splitData.length; i++) {
     total = 0
   }
 }
+console.log(newArr, 'new array with total')
+
+//get the max in the total array
 let numb = Math.max(...newArr)
-console.log(numb)
+console.log(numb, 'max num')
+
+//sort the array in ascending order
+const asceArr = newArr.sort((a, b) => b - a)
+console.log(asceArr, 'ascending')
+
+//get top 3 and add them together
+let topThreeTotal = 0
+for (let i = 0; i < 3; i++) {
+  console.log(asceArr[i])
+  topThreeTotal = asceArr[i] + topThreeTotal
+}
+console.log(topThreeTotal, 'topthreetotal')
